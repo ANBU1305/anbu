ee1c538ca886df1913b4ebdda6aa444becd4309b
ANBU1305 <53506246+ANBU1305@users.noreply.github.com>
2019-08-01 07:19:29 +0530

    Create n array.py

---
+++ n array.py
@@ -0,0 +1,19 @@
+def sumKRepeating(arr, n, k): 
+    sum = 0 
+    visited = [False for i in range(n)] 
+    for i in range(0, n, 1): 
+        if (visited[i] == True): 
+            continue 
+        count = 1
+        for j in range(i + 1, n, 1): 
+            if (arr[i] == arr[j]): 
+                count += 1
+                visited[j] = True
+        if (count == k): 
+            sum += arr[i] 
+    return sum
+if __name__ == '__main__': 
+    arr = [9, 9, 10, 11, 8, 8, 9, 8] 
+    n = len(arr) 
+     k = 3
+    print(sumKRepeating(arr, n, k))
